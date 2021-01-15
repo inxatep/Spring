96d52f38ec7717a30965ee5568ee17f6b1b033cd
Artur Molokov <inxatepcheg@gmail.com>
2021-01-15 23:01:06 +0400

    SpringSec
    (cherry picked from commit be71394e22715388a8305c9fcda98865b0c5f12f)

---
+++ Java/springSec/src/main/java/web/controller/UserController.java
@@ -0,0 +1,28 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import web.service.UserService;
+
+
+@Controller
+@RequestMapping("/user/")
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+
+	@GetMapping(value = "hello")
+	public String printUserInfo(Model model) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		model.addAttribute("user", userService.getUserByName(auth.getName()));
+
+		return "hello";
+	}
+}
---
+++ Java/springSec/src/main/java/web/service/UserService.java
@@ -0,0 +1,22 @@
+package web.service;
+
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    void add(User user);
+
+    void delete(Long id);
+
+    void update (User user);
+
+    List<User> getAll();
+
+    User getById(Long id);
+
+    User getUserByName(String name);
+
+}
