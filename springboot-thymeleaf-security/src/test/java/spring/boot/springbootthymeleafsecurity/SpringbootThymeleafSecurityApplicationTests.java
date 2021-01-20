9a05e108885d8cc40261ad50d63599f1727ff184
Artur Molokov <inxatepcheg@gmail.com>
2021-01-20 15:28:21 +0400

    SpringBoot

---
+++ springboot-thymeleaf-security/src/test/java/spring/boot/springbootthymeleafsecurity/SpringbootThymeleafSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.boot.springbootthymeleafsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
